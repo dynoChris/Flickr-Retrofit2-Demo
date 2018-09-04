04472f2e49cc42e23ad6f5db677de5eadeb0300c
pringleActivity <pringle_activity@outlook.com>
2018-09-04 21:56:12 +0300

    minor update

--- app/src/main/java/com/example/vadym/retrtest1/ApiService.java
+++ app/src/main/java/com/example/vadym/retrtest1/ApiService.java
@@ -9,7 +9,10 @@ public interface ApiService {
     public static final String BASE_URL = "https://api.flickr.com";
     public static final String API_KEY = "5716bb95477866da706c0f552a1627ca";
 
-    @GET("/services/rest")
+    //full response
+    //https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=5716bb95477866da706c0f552a1627ca&format=json&nojsoncallback=1&extras=url_s
+
+    @GET("/services/rest/")
     Call<PhotoResponse> getRecent(
             @Query("method") String method,
             @Query("api_key") String api_key,
